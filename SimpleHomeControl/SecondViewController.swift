646801cbdadc426d7912a4c508ccd68f5b94d3b9
Christoph Eicke <christoph.eicke@fortytools.com>
2016-01-12 08:34:00 +0100

    Initial Commit

---
+++ SimpleHomeControl/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SimpleHomeControl
+//
+//  Created by Christoph Eicke on 12.01.16.
+//  Copyright © 2016 Christoph Eicke. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
